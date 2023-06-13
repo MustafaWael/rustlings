693c24451ef23c4e136f0be373226bccfc428657
Mustafa Wael <mustafa.wael.dev@gmail.com>
2023-06-13 21:12:52 +0000

    Complete the iterators exerciess

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -13,6 +11,21 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    // 1..=num is a range that includes the number num
+    // fold is a function that takes an initial value and a closure with two arguments
+    // the closure takes an accumulator and an element and returns the next accumulator
+    // the closure is called for each element in the range
+    // the final accumulator is returned
+    // the closure multiplies the accumulator by the element
+
+    // num = 4        acc   x
+    // 1  = 1 * 1  <=  1  *  1
+    // 2  = 1 * 2  <=  1  *  2
+    // 6  = 2 * 3  <=  2  *  3
+    // 24 = 6 * 4  <=  6  *  4
+    (1..=num).fold(1, |acc, x| acc * x)
+
 }
 
 #[cfg(test)]
